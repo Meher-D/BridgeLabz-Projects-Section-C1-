c84c00e239162e2851257c92303c3fedd61872a8
Meher-D <dikshikameher22@gmail.com>
2019-08-28 16:12:59 +0530

    2D Array

---
+++ src/com/bridgelabz/arrays/AnagramNPalindrome.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.arrays;
+
+import com.bridgelabz.utility.Utility;
+
+public class AnagramNPalindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+ Utility utility=new Utility();
+ System.out.println("Anagram Numbers:-");
+		utility.primeAnagrams();
+		System.out.println("Palindrome Numbers");
+		utility.primePallindrome();
+	}
+
+}
--- src/com/bridgelabz/utility/Utility.java
+++ src/com/bridgelabz/utility/Utility.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.utility;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -232,7 +233,102 @@ public String inputString() {
 	return scanner.nextLine();
 }
 
+public static void primeAnagrams()
+{
+	ArrayList<Integer> ar = new ArrayList<Integer>();
+	System.out.println();
+	boolean b;
+	for (int j = 2; j <= 1000; j++) 
+	{
+		b = true;
+		for (int i = 2; i < j / 2; i++) 
+		{
+			if (j % i == 0) 
+			{
+				b = false;
+				break;
+			}
+		}
+		if (b)
+			ar.add(j);
+	}
+	for (int i = 0; i < ar.size(); i++) 
+	{
+		for (int j = i + 1; j < ar.size(); j++)
+		{
+			if (anagram(ar.get(i), ar.get(j))) 
+			{
+				System.out.println(ar.get(i) + "  " + ar.get(j));
+			}
+		}
+	}
+}
 
+
+public static boolean anagram(int n1, int n2) 
+{
+	int[] n1count = count(n1);
+	int[] n2count = count(n2);
+	for (int i = 0; i < n2count.length; i++) 
+	{
+		if (n1count[i] != n2count[i]) 
+		{
+			return false;
+		}
+	}
+	return true;
+}
+public static int[] count(int n) 
+{
+	int[] count = new int[10];
+	int temp = n;
+	while (temp != 0)
+	{
+		int r = temp % 10;
+		count[r]++;
+		temp = temp / 10;
+	}
+	return count;
+}
+
+public static void primePallindrome() 
+{
+	System.out.println();
+	boolean b;
+	System.out.println("Palindrome Numbers:");
+	for (int j = 2; j <= 1000; j++) 
+	{
+		b = true;
+		for (int i = 2; i < j / 2; i++) 
+		{
+			if (j % i == 0) 
+			{
+				b = false;
+				break;
+			}
+		}
+		if (b && isPallindrome(j))
+			
+			System.out.print(j + " ");
+	}
+}
+public static boolean isPallindrome(int n) 
+{
+	int temp = n;  
+	int sum=0;
+	  while(n>0)
+	  {    
+	 int r=n%10;  
+	   sum=(sum*10)+r;    
+	   n=n/10;    
+	  }    
+	  if(temp==sum)    
+	   return true; 
+	  else    
+	   return false;
+
+	
+}
 
 
 
